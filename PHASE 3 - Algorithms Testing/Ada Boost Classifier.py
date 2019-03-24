13a57ada48d68be44420463f8129a95538952d57
Stefanos <bluemindset@outlook.com>
2019-03-24 17:31:25 +0200

    Added ADA

--- PHASE 3 - Algorithms Testing/Ada Boost Classifier.py
+++ PHASE 3 - Algorithms Testing/Ada Boost Classifier.py
@@ -5,13 +5,12 @@ Created on Fri Mar 22 00:03:44 2019
 @author: Stefanos Ioannou 
 """
 
-
 from SelectedData import *  
-from sklearn import tree
+from sklearn.ensemble import AdaBoostClassifier
 
-clf = tree.DecisionTreeClassifier()
+clf = AdaBoostClassifier(base_estimator=None, n_estimators=50, learning_rate=1.0, algorithm='SAMME.R', random_state=None)
 clf = clf.fit(xtrain, ytrain)
 ynew = clf.predict(xtest)
 print(accuracy_score(ynew, ytest))
 
-#Result : 0.7324420677361854 %
+#Result : 0.8003565062388592 %
