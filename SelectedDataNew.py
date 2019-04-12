6756da1401f84ca81ee462928f21f32dd0e5c5db
Stephanos Ioannou <bluemindset@outlook.com>
2019-04-12 21:34:09 +0300

    Add files via upload

--- SelectedDataNew.py
+++ SelectedDataNew.py
@@ -12,13 +12,16 @@ from sklearn.metrics import accuracy_score
 from sklearn.datasets import make_classification
 from sklearn.metrics import classification_report
 
-test=pd.read_csv("test.csv")
-train=pd.read_csv("train.csv")
+test=pd.read_csv("testcomb.csv")
+train=pd.read_csv("traincomb.csv")
 
-xtrain=train.iloc[:,1:15]
+#test=pd.read_csv("Time/newtesttime.csv")
+#train=pd.read_csv("Time/newtraintime.csv")
+
+xtrain=train.iloc[:,1:17]
 ytrain=train['res']
 
-xtest=test.iloc[:,1:15]
+xtest=test.iloc[:,1:17]
 ytest=test['res']
 
 
